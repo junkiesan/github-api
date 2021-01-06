f16693ba6f2cf97d8b70785f7b9ca16bcc1855dc
Julien Bouland <bouland.julien@gmail.com>
2021-01-06 19:31:31 +0100

    results appends under input

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+// Get the GitHub username input form
+const gitHubForm = document.getElementById('gitHubForm');
+
+// listen for submissions on gitHub username input form
+gitHubForm.addEventListener('submit', (e) => {
+
+  // prevent default form submission action
+  e.preventDefault();
+
+  // get the gitHub username input field on the DOM
+  let usernameInput = document.getElementById('usernameInput');
+
+  // get the value of the gitHub username input field
+  let gitHubUsername = usernameInput.value;
+
+  // run gitHub API function, passing in the gitHub username
+  requestUserRepos(gitHubUsername);
+})
+
+
 function requestUserRepos(username){
   // create a new XMLHttpRequest object
   const xhr = new XMLHttpRequest();
@@ -15,25 +35,26 @@ function requestUserRepos(username){
 
     // parse API data into JSON
     const data = JSON.parse(this.response);
-    // log the response
-    console.log(data);
 
     // loop over each object in data array
     for (let  i in data) {
-      // log the repo name
-      console.log('Repo:', data[i].name);
-      // log the repo description
-      console.log('Repo:', data[i].description);
-      // log the repo url
-      console.log('Repo:', data[i].html_url);
-      // add a separator
-      console.log('====================');
+      // Get the ul with id of of userRepos
+      let ul = document.getElementById('userRepos');
+      // Create variable that will create li's to be added to ul
+      let li = document.createElement('li');
+      // Add Bootstrap list item class to each li
+      li.classList.add('list-group-item');
+      // Create the html markup for each li
+      li.innerHTML = (`
+        <p><strong>Repo:</strong> ${data[i].name}</p>
+        <p><strong>Description:</strong> ${data[i].description}</p>
+        <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+      `);
+      // Append each li to the ul
+      ul.appendChild(li);
     }
   }
 
   // send the request to the server
   xhr.send();
 }
-
-// call function with a username
-requestUserRepos('junkiesan');
