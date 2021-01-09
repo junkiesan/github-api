4ad0b7b72d0e1bdabc356219c5abf8c04f25be6e
Julien Bouland <bouland.julien@gmail.com>
2021-01-09 19:09:33 +0100

    print number of repos

--- app.js
+++ app.js
@@ -40,22 +40,42 @@ function requestUserRepos(username){
       root.removeChild(root.firstChild);
     }
 
-    // loop over each object in data array
-    for (let  i in data) {
+    if (data.message === "Not Found") {
       // Get the ul with id of of userRepos
-      let ul = document.getElementById('userRepos');
+      let ul = document.getElementById('userRepos');      // loop over each object in data array
       // Create variable that will create li's to be added to ul
       let li = document.createElement('li');
       // Add Bootstrap list item class to each li
       li.classList.add('list-group-item');
       // Create the html markup for each li
       li.innerHTML = (`
-        <p><strong>Repo:</strong> ${data[i].name}</p>
-        <p><strong>Description:</strong> ${data[i].description}</p>
-        <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
-      `);
+        <p><strong>No account exists with the username:</strong> ${username}</p>`);
       // Append each li to the ul
       ul.appendChild(li);
+
+    }  else {
+      // Get the ul with id of of userRepos
+      let ul = document.getElementById('userRepos');
+      // Create variable that will create li's to be added to ul
+      let p = document.createElement('p');
+      // Add Bootstrap list item class to each li
+      p.innerHTML = (`<p><strong>Number of Public Repos:${data.length}</p>`)
+      ul.appendChild(p);
+      // Loop over each object in data array
+      for (let i in data) {
+        // Create variable that will create li's to be added to ul
+        let li = document.createElement('li');
+        // Add Bootstrap list item class to each li
+        li.classList.add('list-group-item');
+        // Create the html markup for each li
+        li.innerHTML = (`
+          <p><strong>Repo:</strong> ${data[i].name}</p>
+          <p><strong>Description:</strong> ${data[i].description}</p>
+          <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+        `);
+        // Append each li to the ul
+        ul.appendChild(li);
+      }
     }
   }
 
